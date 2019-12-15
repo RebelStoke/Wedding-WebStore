509ef149e02142261b54c775dfd754ad7266c90c
Nedas Surkus <nedassurkus@gmail.com>
2019-12-15 16:52:10 +0100

    Update DateService.cs

--- BackEnd/WeddingApp/WeddingApp.Core/ApplicationService/ImplementedService/DateService.cs
+++ BackEnd/WeddingApp/WeddingApp.Core/ApplicationService/ImplementedService/DateService.cs
@@ -22,7 +22,7 @@ namespace WeddingApp.Core.ApplicationService.ImplementedService
 
         public List<DatesAssigned> GetAllDatesForMonth(int year ,int month)
         {
-            if (month < 0 || month > 12)
+            if (month <= 0 || month > 12)
             {
                 throw new InvalidDataException("Month is invalid");
             }
